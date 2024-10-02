5ea2f0a7fc472e2425adac72a5ed1bf8185fb013
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-10-02 11:00:01 +0200

    template pipelines and installation instructions

--- package_installation.R
+++ package_installation.R
@@ -1,23 +1,66 @@
+install.packages("remotes") # can install from github
+install.packages("paleoTS") # paleontological time series
+
+
+
 remotes::install_github(repo = "MindTheGap-ERC/admtools",
                         build_vignettes = TRUE,
-                        ref = "dev",
                         dependencies = TRUE)
+
 remotes::install_github(repo = "MindTheGap-ERC/StratPal",
                         build_vignettes = TRUE,
                         ref = "dev",
                         dependencies = TRUE)
 
-library(StratPal)
-library(admtools)
+library(StratPal) # biology part
+library(admtools) # age-depth
+library(paleoTS)  # time series analysis
+
+x = paleoTS::sim.GRW()
+plot(x)
+
+# fit models
+fit3models(x)
+fit9models(x)
+
+?`paleoTS-package`
+fitSimple(x, model = "OU")
+# fits only one model, have to do model comparison afterwards
+# inspect the code of fit3models
+fit3models # ctl + enter
+
+
+## new stratpal functionality
 
 adm = tp_to_adm(scenarioA$t_myr, scenarioA$h[,"2km"])
 
 plot(adm)
+# paleots is a summary
 
-x = stasis_sl(scenarioA$t_myr)
+# pre-paleoTS
 
-plot(reduce_to_paleoTS.pre_paleoTS(x))
+x = stasis_sl(1:3) # sl stand for specimen level
+x = random_walk_sl()
+?random_walk_sl
 
-a = x |> time_to_strat(adm) |> reduce_to_paleoTS.pre_paleoTS() |> plot(xlab = "Stratigraphic Position")
+x$t # times
+x$vals # individual trait values
+
+# plotting pipeline
+seq(0.1, 1.9, by  = 0.1) |>
+  random_walk_sl() |>
+  time_to_strat(adm) |>
+  reduce_to_paleoTS() |>
+  plot()
+
+# use `reduce_to_paleoTS` before plotting
+?reduce_to_paleoTS
+
+# analysis pipeline
+seq(0.1, 1.9, by  = 0.1) |>
+  random_walk_sl() |>
+  time_to_strat(adm) |>
+  reduce_to_paleoTS() |>
+  fit3models()
 
 
