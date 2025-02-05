43f60b9365e5414397a9a47dc9990c7b0601b454
SidBick <sidneybickerton@gmail.com>
2025-02-05 13:47:02 +0100

    Update 3.7_adm_last occurence.R

--- 3.7_adm_last occurence.R
+++ 3.7_adm_last occurence.R
@@ -14,27 +14,48 @@ t_mod_shifted = t_mod - min(t_mod)
 h4 = data_kitten$adm8..m.                                  # 4km adm
 h4_mod = h4[ t >= 1.25 & t <= 3.25]
 h4_mod_shifted = h4_mod - min(h4_mod)
-
 adm4_mod = tp_to_adm(t = t_mod, h4_mod)                    # adm modified to show 1.25-3.25 Myr
 adm_4km = tp_to_adm(t = t_mod_shifted, h4_mod_shifted)     # adm modified & shifted
-plot(adm4_mod)
-plot(adm_4km)
-title('4km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+
+plot(adm_4km)                                    
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty="dashed")
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty="dashed")
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty="dashed")
+title(main = "age depth model 4km",
+      xlab = "time [Myr]", ylab = "height [m]",
+      cex.main = 1.2)
+text(x=0.21, y=40, "Lowstand 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=0.7, y=80, "Transgressive 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.2, y=20, "Highstand
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.7, y=60, "Falling stage 
+     system tract",cex=0.8,col='darkblue',font=3)
+
 ###         ###  9 km       adm
 h9 = data_kitten$adm18..m.                                  # 9km adm
 h9_mod = h9[ t >= 1.25 & t <= 3.25]
 h9_mod_shifted = h9_mod - min(h9_mod)
-
 adm9_mod = tp_to_adm(t = t_mod, h9_mod)                    # adm modified to show 1.25-3.25 Myr
 adm_9km = tp_to_adm(t = t_mod_shifted, h9_mod_shifted)     # adm modified & shifted
-plot(adm_9km)
-title('9km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+
+plot(adm_9km)                                     
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty="dashed")
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty="dashed")
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty="dashed")
+title(main = "age depth model 9km",
+      xlab = "time [Myr]", ylab = "height [m]",
+      cex.main = 1.2)
+text(x=0.14, y=30, "Lowstand 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=0.7, y=50, "Transgressive 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.2, y=70, "Highstand
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.7, y=90, "Falling stage 
+     system tract",cex=0.8,col='darkblue',font=3)
+
 ###test 10km adm
 h10 = data_kitten$adm20..m.                                # 10km adm
 h10_mod = h10[ t >= 1.25 & t <= 3.25]
@@ -43,9 +64,9 @@ adm10_mod = tp_to_adm(t = t_mod, h10_mod)
 adm_10km = tp_to_adm(t = t_mod_shifted, h10_mod_shifted)   
 plot(adm_10km)
 title('10km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty='dashed')
 ###test 11km adm
 h11 = data_kitten$adm22..m.                                # 11km adm
 h11_mod = h11[ t >= 1.25 & t <= 3.25]
@@ -54,9 +75,9 @@ adm11_mod = tp_to_adm(t = t_mod, h11_mod)
 adm_11km = tp_to_adm(t = t_mod_shifted, h11_mod_shifted)   
 plot(adm_11km)
 title('11km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty='dashed')
 ###         ###  12 km    adm
 h12 = data_kitten$adm24..m.                                # 12km adm
 h12_mod = h12[ t >= 1.25 & t <= 3.25]
@@ -66,9 +87,20 @@ adm12_mod = tp_to_adm(t = t_mod, h12_mod)                  # adm modified to sho
 adm_12km = tp_to_adm(t = t_mod_shifted, h12_mod_shifted)   # adm modified & shifted
 plot(adm_12km)
 title('12km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty='dashed')
+title(main = "age depth model 12km",
+      xlab = "time [Myr]", ylab = "height [m]",
+      cex.main = 1.2)
+text(x=0.21, y=40, "Lowstand 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=0.7, y=50, "Transgressive 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.2, y=60, "Highstand
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.7, y=70, "Falling stage 
+     system tract",cex=0.8,col='darkblue',font=3)
 ###         ### 13 km     adm
 h13 = data_kitten$adm26..m.                                # 13km adm
 h13_mod = h13[ t >= 1.25 & t <= 3.25]
@@ -78,9 +110,9 @@ adm13_mod = tp_to_adm(t = t_mod, h13_mod)                  # adm modified to sho
 adm_13km = tp_to_adm(t = t_mod_shifted, h13_mod_shifted)   # adm modified & shifted
 plot(adm_13km)
 title('13km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty='dashed')
 ###         ###  15 km    adm
 h15 = data_kitten$adm30..m.                                # 15km adm
 h15_mod = h15[ t >= 1.25 & t <= 3.25]
@@ -90,9 +122,20 @@ adm15_mod = tp_to_adm(t = t_mod, h15_mod)                  # adm modified to sho
 adm_15km = tp_to_adm(t = t_mod_shifted, h15_mod_shifted)   # adm modified & shifted
 plot(adm_15km,lwd_destr = 0)
 title('15km adm')
-abline(v=(LST_to_TST),col="red",lwd=2,lty='dashed')
-abline(v=(TST_to_HST),col="red",lwd=2,lty='dashed')
-abline(v=(HST_to_FSST),col="red",lwd=2,lty='dashed')
+abline(v=(LST_to_TST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(TST_to_HST),col="cyan4",lwd=3,lty='dashed')
+abline(v=(HST_to_FSST),col="cyan4",lwd=3,lty='dashed')
+title(main = "age depth model 15km",
+      xlab = "time [Myr]", ylab = "height [m]",
+      cex.main = 1.2)
+text(x=0.2, y=15, "Lowstand 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=0.72, y=5, "Transgressive 
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.2, y=10, "Highstand
+     system tract",cex=0.8,col='darkblue',font=3)
+text(x=1.7, y=15, "Falling stage 
+     system tract",cex=0.8,col='darkblue',font=3)
 
 ### example of previous adm code:
 ## adm_6km = tp_to_adm(t = data_kitten$time..Myr., data_kitten$adm12..m.)
@@ -123,15 +166,29 @@ HST_to_FSST = 1.5     # Highstand system tract to Falling Stage system tract
 p3(rate = 500, from = min_time(adm_4km), to = max_time(adm_4km)) |>    # constant rate in time domain
   time_to_strat(adm_4km, destructive = FALSE) |>                       # transform into depth domain
   hist(main = "Last occurrence at 4 km with constant extinction rate",
-       sub = "system tracts seperated by vertical lines",
        ylab = "Last occurrence",
        xlab = "Stratigraphic height [m]",
        breaks = seq(from = min_height(adm_4km), to = max_height(adm_4km), length.out = 100),
        col="white")
-abline(v=(time_to_strat(LST_to_TST,adm_4km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(TST_to_HST,adm_4km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(HST_to_FSST,adm_4km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-strat_to_time(7.5,adm_4km)
+abline(v=(time_to_strat(LST_to_TST,adm_4km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(TST_to_HST,adm_4km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(HST_to_FSST,adm_4km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+text(x=8, y=95, "LST",cex=0.9,col='darkblue',font=3)
+text(x=45, y=95, "TST",cex=0.9,col='darkblue',font=3)
+text(x=82, y=95, "HST",cex=0.9,col='darkblue',font=3)
+text(x=95, y=95, "FSST",cex=0.9,col='darkblue',font=3)
+
+ggplot(df,aes(x))+geom_histogram(bins=30)+coord_flip()
+?aes
+adm_4km[["h"]]
+dft<-data.frame(adm_4km[["t"]])
+dfh<-data.frame(adm_4km[["h"]])
+freq<-p3(rate = 500, from = min_time(adm_4km), to = max_time(adm_4km) |>   
+  time_to_strat(adm_4km, destructive = FALSE)
+df <-data.frame(adm_4km[["t"]],adm_4km[["h"]],freq)
+ggplot(df,aes(adm_4km[["t"]],adm_4km[["h"]])+geom_line(colour="black")
+
+
 
 ### Fossil abundance at adm_9km
 p3(rate = 500, from = min_time(adm_9km), to = max_time(adm_9km)) |>    # constant rate in time domain
@@ -142,11 +199,15 @@ p3(rate = 500, from = min_time(adm_9km), to = max_time(adm_9km)) |>    # constan
        xlab = "Stratigraphic height [m]",
        breaks = seq(from = min_height(adm_9km), to = max_height(adm_9km), length.out = 100),
        col="white")
-abline(v=(time_to_strat(LST_to_TST,adm_9km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(TST_to_HST,adm_9km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(HST_to_FSST,adm_9km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-time_to_strat(0.25,adm_9km,destructive=FALSE)
-abline(v=(5.515),col="coral",lwd=3,lty='dashed')
+abline(v=(time_to_strat(LST_to_TST,adm_9km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(TST_to_HST,adm_9km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(HST_to_FSST,adm_9km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+text(x=25, y=95, "LST",cex=0.9,col='darkblue',font=3)
+text(x=90, y=95, "TST",cex=0.9,col='darkblue',font=3)
+text(x=135, y=95, "HST",cex=0.9,col='darkblue',font=3)
+text(x=157, y=95, "FSST",cex=0.9,col='darkblue',font=3)
+#time_to_strat(0.25,adm_9km,destructive=FALSE)
+#abline(v=(5.515),col="coral",lwd=3,lty='dashed')
 
 ### Fossil abundance at adm_12km
 p3(rate = 500, from = min_time(adm_12km), to = max_time(adm_12km)) |>  # constant rate in time domain
@@ -157,9 +218,13 @@ p3(rate = 500, from = min_time(adm_12km), to = max_time(adm_12km)) |>  # constan
        xlab = "Stratigraphic height [m]",
        breaks = seq(from = min_height(adm_12km), to = max_height(adm_12km), length.out = 100),
        col="white")
-abline(v=(time_to_strat(LST_to_TST,adm_12km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(TST_to_HST,adm_12km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(HST_to_FSST,adm_12km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
+abline(v=(time_to_strat(LST_to_TST,adm_12km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(TST_to_HST,adm_12km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(HST_to_FSST,adm_12km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+text(x=4, y=79, "LST",cex=0.9,col='darkblue',font=3)
+text(x=13, y=79, "TST",cex=0.9,col='darkblue',font=3)
+text(x=21, y=79, "HST",cex=0.9,col='darkblue',font=3)
+text(x=40, y=79, "FSST",cex=0.9,col='darkblue',font=3)
 
 ### Fossil abundance at adm_15km
 p3(rate = 500, from = min_time(adm_15km), to = max_time(adm_15km)) |>  # constant rate in time domain
@@ -170,10 +235,13 @@ p3(rate = 500, from = min_time(adm_15km), to = max_time(adm_15km)) |>  # constan
        xlab = "Stratigraphic height [m]",
        breaks = seq(from = min_height(adm_15km), to = max_height(adm_15km), length.out = 100),
        col="white")
-abline(v=(time_to_strat(LST_to_TST,adm_15km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(TST_to_HST,adm_15km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-abline(v=(time_to_strat(HST_to_FSST,adm_15km,destructive=FALSE)),col="red",lwd=2,lty='dashed')
-
+abline(v=(time_to_strat(LST_to_TST,adm_15km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(TST_to_HST,adm_15km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+abline(v=(time_to_strat(HST_to_FSST,adm_15km,destructive=FALSE)),col="cyan4",lwd=3,lty='dashed')
+text(x=4, y=100, "LST",cex=0.9,col='darkblue',font=3)
+text(x=13, y=100, "TST",cex=0.9,col='darkblue',font=3)
+text(x=22, y=100, "HST",cex=0.9,col='darkblue',font=3)
+text(x=30, y=100, "FSST",cex=0.9,col='darkblue',font=3)
 ########## Eustatic Sea Level
 AMP1 = 15
 PER1 = 2
@@ -186,10 +254,17 @@ sl = (AMP1*sin(2*pi*t/PER1)) + (AMP2*sin(2*pi*t/PER2))
   main="Eustatic Sea Level",
   xlab="time",
   ylab="meter")
-abline(v=1.75,col="cyan3",lwd=3,lty='dashed')
-abline(v=2.25,col="cyan3",lwd=3,lty='dashed')
-abline(v=2.75,col="cyan3",lwd=3,lty='dashed')
-abline(v=(0.32+1.25),col="red",lwd=2,lty='dashed')
+abline(v=1.75,col="cyan4",lwd=3,lty='dashed')
+abline(v=2.25,col="cyan4",lwd=3,lty='dashed')
+abline(v=2.75,col="cyan4",lwd=3,lty='dashed')
+text(x=1.4, y=0, "Lowstand 
+     system tract",cex=0.9,col='darkblue',font=3)
+text(x=2, y=12, "Transgressive 
+     system tract",cex=0.9,col='darkblue',font=3)
+text(x=2.5, y=0, "Highstand 
+     system tract",cex=0.9,col='darkblue',font=3)
+text(x=3, y=12, "Falling stage 
+     system tract",cex=0.9,col='darkblue',font=3)
 
 ############### Scenario 1 Lowstand System Tract
 ### time: 1.25 - 1.75 Myr   #shifted: 0 - 0.5 Myr
@@ -571,67 +646,4 @@ plot(tp,ext_rate,type='l',lwd=3,                  # graph simply showing the ext
   xlab="time",
   ylab="extinction rate")
 
-plot(adm_4km)                                     # 4km plot of age depth model
-abline(v=(LST_to_TST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(TST_to_HST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(HST_to_FSST),col="cyan4",lwd=2,lty="dashed")
-title(main = "age depth model 4km",
-      xlab = "time [Myr]", ylab = "height [m]",
-      cex.main = 1.2)
-text(x=0.21, y=40, "Lowstand 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=0.7, y=80, "Transgressive 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.2, y=20, "Highstand
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.7, y=60, "Falling stage 
-     system tract",cex=0.8,col='darkblue',font=3)
-
-plot(adm_9km)                                     # 9km
-abline(v=(LST_to_TST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(TST_to_HST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(HST_to_FSST),col="cyan4",lwd=2,lty="dashed")
-title(main = "age depth model 9km",
-      xlab = "time [Myr]", ylab = "height [m]",
-      cex.main = 1.2)
-text(x=0.14, y=30, "Lowstand 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=0.7, y=50, "Transgressive 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.2, y=70, "Highstand
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.7, y=90, "Falling stage 
-     system tract",cex=0.8,col='darkblue',font=3)
-
-plot(adm_12km)                                     # 12km
-abline(v=(LST_to_TST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(TST_to_HST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(HST_to_FSST),col="cyan4",lwd=2,lty="dashed")
-title(main = "age depth model 12km",
-      xlab = "time [Myr]", ylab = "height [m]",
-      cex.main = 1.2)
-text(x=0.21, y=40, "Lowstand 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=0.7, y=50, "Transgressive 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.2, y=60, "Highstand
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.7, y=70, "Falling stage 
-     system tract",cex=0.8,col='darkblue',font=3)
-
-plot(adm_15km)                                     # 15km
-abline(v=(LST_to_TST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(TST_to_HST),col="cyan4",lwd=2,lty="dashed")
-abline(v=(HST_to_FSST),col="cyan4",lwd=2,lty="dashed")
-title(main = "age depth model 15km",
-      xlab = "time [Myr]", ylab = "height [m]",
-      cex.main = 1.2)
-text(x=0.2, y=15, "Lowstand 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=0.72, y=5, "Transgressive 
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.2, y=10, "Highstand
-     system tract",cex=0.8,col='darkblue',font=3)
-text(x=1.7, y=15, "Falling stage 
-     system tract",cex=0.8,col='darkblue',font=3)
 
