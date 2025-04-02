3761660d80f5ddbf9c954c19e14c7482bbbf8231
Emilia Jarochowska <e.b.jarochowska@uu.nl>
2025-04-02 22:19:45 +0200

    add export of water depth

--- src/extract_data.jl
+++ src/extract_data.jl
@@ -7,14 +7,15 @@ using CarboKitten.Export: data_export, CSV
 const TAG = "example_Anna"
 const PATH = "data"
 
-function plot_model()
+function main()
 data_export(
         CSV(tuple.(10:20:70, 25),
             :age_depth_model => "$(PATH)/$(TAG)_adm.csv",
+            :water_depth => "$(PATH)/$(TAG)_wd.csv",
             :metadata => "$(PATH)/$(TAG).toml"),
         "$(PATH)/$(TAG).h5")
 end
 
 end
 
-Extract_data.plot_model()
+Extract_data.main()
