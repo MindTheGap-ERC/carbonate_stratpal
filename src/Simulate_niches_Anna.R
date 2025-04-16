206c4f758e3f15b791b27f7f9b8a221b52b2724f
annAjnsn <135852286+annAjnsn@users.noreply.github.com>
2025-04-16 12:29:37 +0200

    Add files via upload

---
+++ src/Simulate_niches_Anna.R
@@ -0,0 +1,5 @@
+library(StratPal) #bio and ecology
+library(admtools) #stratigraphy
+
+#task is to create 100 niches described by water depth from CarboKitten model
+#Could I create a code that will result in the generation of a set of 100 columns with each the data points for the different niches, but indicating that the first and last have their optima at the shallowest and deepest points, and that until x depth the niches are small, but then become larger, and where the niches themselves are described by standard distributions?
