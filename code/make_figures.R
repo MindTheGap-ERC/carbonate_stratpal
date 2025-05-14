bf9fcbbf0a8a5f0b8fedf9123f07a75e9a154990
SidBick <sidneybickerton@gmail.com>
2025-05-14 11:36:42 +0200

    changed to read latest run

--- code/make_figures.R
+++ code/make_figures.R
@@ -1,6 +1,4 @@
-data_kitten = read.csv("data/alcap-example_adm_10.csv")
-
-file.exists("data/alcap-example_adm_10.csv")
+data_kitten = read.csv("alcap-example_adm_12.csv")
             
 #grid: 20km x 50km
 #time: 4 Myr
