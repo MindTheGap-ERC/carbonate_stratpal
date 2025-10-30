01f357da2ad9184347e033a1e57474f0ec5e7ffc
Niklas Hohmann <n.h.hohmann@uu.nl>
2025-10-30 10:32:25 +0100

    compactify pre-runs to generate initial topography

--- code/doodles.jl
+++
@@ -1,8 +0,0 @@
-    
-    using CarboKitten
-    using Unitful
-    using CairoMakie
-    using CarboKitten.Visualization
-
-    fig = summary_plot("data/init_carbonate_stratpal_1.h5")
-    save("figs/test.png", fig)
--- code/platform.jl
+++ code/platform.jl
@@ -1,35 +1,46 @@
+
 using Unitful
 using CarboKitten
 using CarboKitten.Export: read_slice, data_export, read_volume
 using CarboKitten.Utility: in_units_of
 
-const PATH = "data"
-const TAG = "ramp"
 
-const FACIES_RAMP = [
+const TAG = "platform"
+const PATH = "data"
+const FACIES_PLATFORM = [
     ALCAP.Facies(
         viability_range = (4, 10),
         activation_range = (6, 10),
-        maximum_growth_rate=200u"m/Myr",
-        extinction_coefficient=0.6u"m^-1",
+        maximum_growth_rate=500u"m/Myr",
+        extinction_coefficient=0.8u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=80.0u"m/yr"),
+        diffusion_coefficient=50.0u"m/yr"),
     ALCAP.Facies(
         viability_range = (4, 10),
         activation_range = (6, 10),
-        maximum_growth_rate=300u"m/Myr",
+        maximum_growth_rate=400u"m/Myr",
         extinction_coefficient=0.1u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=100.0u"m/yr"),
+        diffusion_coefficient=25.0u"m/yr"),
     ALCAP.Facies(
         viability_range = (4, 10),
         activation_range = (6, 10),
-        maximum_growth_rate=50u"m/Myr",
+        maximum_growth_rate=100u"m/Myr",
         extinction_coefficient=0.005u"m^-1",
         saturation_intensity=60u"W/m^2",
-        diffusion_coefficient=30u"m/yr")
+        diffusion_coefficient=12.5u"m/yr")
 ]
 
+const PERIOD1 = 2.0u"Myr"
+const AMPLITUDE1 = 15.0u"m"
+const PERIOD2 = 0.2u"Myr"
+const AMPLITUDE2 = 2.5u"m"
+const DELTA_T = 100u"yr"
+const STEPS_INIT = 10000
+const STEPS_MAIN = 40000
+const PHYS_SCALE = 150u"m"
+const BOX = Box{Coast}(grid_size=(150, 50), phys_scale=PHYS_SCALE)
+const SUBSIDENCE_RATE = 30u"m/Myr"
 
 function get_init_topography(header, volume)    
     n_steps = size(volume.sediment_thickness)[3]
@@ -39,30 +50,30 @@ function get_init_topography(header, volume)
     result[:, :] = (volume.sediment_thickness[:,:,n_steps] .+ bedrock) |> in_units_of(u"m")
 end
 
-const PERIOD1 = 2.0u"Myr"
-const AMPLITUDE1 = 20.0u"m"
-const PERIOD2 = 0.2u"Myr"
-const AMPLITUDE2 = 2.0u"m"
+
+
+
+
 
 const INPUT_INIT_RUN = ALCAP.Input(
     tag="$TAG",
-    box=Box{Coast}(grid_size=(150, 50), phys_scale=150.0u"m"),
+    box=BOX,
     time=TimeProperties(
-        Δt=100u"yr",
-        steps=10000),
+        Δt=DELTA_T,
+        steps=STEPS_INIT),
     output=Dict(
         :profile => OutputSpec(slice=(:, 25), write_interval=10),
         :topography => OutputSpec(slice = (:, :), write_interval = 1000)),
     ca_interval=1,
     initial_topography=(x, y) -> -x / 300.0,
     sea_level=t -> 0u"m",
-    subsidence_rate=30.0u"m/Myr",
+    subsidence_rate=SUBSIDENCE_RATE,
     disintegration_rate=50.0u"m/Myr",
     insolation=400.0u"W/m^2",
     sediment_buffer_size=50,
     depositional_resolution=500.0u"m",
-    cementation_time = 1u"kyr",
-    facies=FACIES_RAMP)
+    facies=FACIES_PLATFORM,
+    cementation_time = 1u"kyr")
 
 println("Running initial model\n")
 run_model(Model{ALCAP}, INPUT_INIT_RUN, "$(PATH)/$(TAG)_prerun.h5")
@@ -71,26 +82,26 @@ header, volume = read_volume("$(PATH)/$(TAG)_prerun.h5", :topography)
 
 init_matrix = get_init_topography(header, volume)
 
-
 const INPUT_MAIN_RUN = ALCAP.Input(
     tag="$TAG",
-    box=Box{Coast}(grid_size=(150, 50), phys_scale=150.0u"m"),
+    box=BOX,
     time=TimeProperties(
-        Δt=100u"yr",
-        steps=40000),
+        Δt=DELTA_T,
+        steps=STEPS_MAIN),
     output=Dict(
         :profile => OutputSpec(slice=(:, 25), write_interval=10),
-        :topography => OutputSpec(slice = (:, :), write_interval = 10)),
+        :topography => OutputSpec(slice = (:, :), write_interval = 100)),
     ca_interval=1,
-    initial_topography=init_matrix* u"m",
+    initial_topography=init_matrix * u"m", #initial_topography1(path),#(x, y) -> -x / 300.0,
     sea_level=t -> AMPLITUDE1 * sin(2π * t / PERIOD1) + AMPLITUDE2 * sin(2π * t / PERIOD2),
-    subsidence_rate=30.0u"m/Myr",
+    subsidence_rate=SUBSIDENCE_RATE,
     disintegration_rate=50.0u"m/Myr",
     insolation=400.0u"W/m^2",
     sediment_buffer_size=50,
     depositional_resolution=500.0u"m",
-    cementation_time = 1u"kyr",
-    facies=FACIES_RAMP)
+    facies=FACIES_PLATFORM,
+    cementation_time = 1u"kyr")
 
 println("Running main model\n")
 run_model(Model{ALCAP}, INPUT_MAIN_RUN, "$(PATH)/$(TAG).h5")
+
